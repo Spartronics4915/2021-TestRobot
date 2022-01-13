d7f0f34e680e32164fa4da10ad6dd81754d87156
ss0g <55930267+ss0g@users.noreply.github.com>
2022-01-12 17:56:59 -0800

    Camden is so cool.

--- src/main/java/com/spartronics4915/frc/commands/ExampleCommand.java
+++ src/main/java/com/spartronics4915/frc/commands/ExampleCommand.java
@@ -25,7 +25,7 @@ public class ExampleCommand extends CommandBase
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        mExampleSubsystem.startTestMotor(0.3);
+        mExampleSubsystem.startTestMotor(1);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
